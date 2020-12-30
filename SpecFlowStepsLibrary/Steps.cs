22052edcd66fcdbef445291ea79e0044f57f121c
Rami Abughazaleh <rami.abughazaleh@gmail.com>
2020-12-29 22:11:04 -0800

    first commit

---
+++ SpecFlowStepsLibrary/Steps.cs
@@ -0,0 +1,24 @@
+﻿using BoDi;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowStepsLibrary
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        private readonly ScenarioContext scenarioContext;
+        private readonly IObjectContainer objectContainer;
+
+        public StepDefinitions(ScenarioContext scenarioContext, IObjectContainer objectContainer)
+        {
+            this.scenarioContext = scenarioContext;
+            this.objectContainer = objectContainer;
+        }
+
+        [Given(@"this is a step in another assembly")]
+        public void GivenThisIsAStepInAnotherAssembly()
+        {
+            scenarioContext.Pending();
+        }
+    }
+}
---
+++ UnitTestProject25/MyFeature.feature
@@ -0,0 +1,4 @@
+﻿Feature: My Feature
+
+Scenario: My scenario
+	Given this is a step in another assembly
